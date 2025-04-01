c0bba2148620d4cabab62bf5cac8efdf015ab09c
Vikalp Nag <vikalpnag@gmail.com>
2025-04-01 09:19:30 +0530

    Escrow test #success

--- Millow2/test/Escrow.js
+++ Millow2/test/Escrow.js
@@ -6,10 +6,36 @@ const tokens = (n) => {
 };
 
 describe("Escrow", () => {
+  let buyer, seller, lender, inspector;
+  let realEstate, escrow;
   it("saves the addresses", async () => {
-    const RealEstate = await ethers.getContractFactory("RealEstate");
-    const realEstate = await RealEstate.deploy();
+    //Setup Accounts
+    [buyer, seller, lender, inspector] = await ethers.getSigners();
 
-    console.log(realEstate);
+    //Deploy Real Estate
+    const RealEstate = await ethers.getContractFactory("RealEstate");
+    realEstate = await RealEstate.deploy();
+
+    //mint a Nft
+    let transaction = await realEstate
+      .connect(seller)
+      .mint(
+        "https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/1.json"
+      );
+    await transaction.wait();
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      seller.address,
+      lender.address,
+      inspector.address
+    );
+
+    const result = await escrow.nftAddress();
+    expect(result).to.be.equal(realEstate.address);
+
+    const result2 = await escrow.seller();
+    expect(result2).to.be.equal(seller.address);
   });
 });
