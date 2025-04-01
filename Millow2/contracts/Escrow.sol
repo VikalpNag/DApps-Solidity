4f01e3501e7e9c3f480b9e0e568b8ec2ff36f594
Vikalp Nag <vikalpnag@gmail.com>
2025-04-01 08:49:28 +0530

    hardhat test success

--- Millow2/contracts/Escrow.sol
+++ Millow2/contracts/Escrow.sol
@@ -2,11 +2,7 @@
 pragma solidity ^0.8.0;
 
 interface IERC721 {
-    function transferFrom(
-        address _from,
-        address _to,
-        uint256 _id
-    ) external;
+    function transferFrom(address _from, address _to, uint256 _id) external;
 }
 
 contract Escrow {
@@ -15,11 +11,15 @@ contract Escrow {
     address public inspector;
     address public lender;
 
-
-    constructor(address _nftAddress,address payable _seller,address _inspector,address _lender){
-        nftAddress= _nftAddress;
-        seller= _seller;
-        inspector= _inspector;
+    constructor(
+        address _nftAddress,
+        address payable _seller,
+        address _inspector,
+        address _lender
+    ) {
+        nftAddress = _nftAddress;
+        seller = _seller;
+        inspector = _inspector;
         lender = _lender;
     }
 }
