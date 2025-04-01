922db2281616927c2ab106638de11baa49a19d8e
Vikalp Nag <vikalpnag@gmail.com>
2025-04-01 16:00:05 +0530

    deposit earnest,onlyseller,onlybuyer

--- Millow2/contracts/Escrow.sol
+++ Millow2/contracts/Escrow.sol
@@ -11,6 +11,21 @@ contract Escrow {
     address public inspector;
     address public lender;
 
+    modifier onlyBuyer(uint256 _nftID) {
+        require(msg.sender == buyer[_nftID], "Only buyer can call this method");
+        _;
+    }
+
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only Seller can call this method");
+        _;
+    }
+
+    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => address) public buyer;
+    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public escrowAmount;
+
     constructor(
         address _nftAddress,
         address payable _seller,
@@ -23,7 +38,23 @@ contract Escrow {
         lender = _lender;
     }
 
-    function list(uint256 _nftID) public {
+    function list(
+        uint256 _nftID,
+        address _buyer,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount
+    ) public payable onlySeller {
+        //Transfer NFT from seller to this contract
         IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+        isListed[_nftID] = true;
+        buyer[_nftID] = _buyer;
+        purchasePrice[_nftID] = _purchasePrice;
+        escrowAmount[_nftID] = _escrowAmount;
+    }
+
+    //Put under contract(only buyer -payable escrow)
+    function depositeEarnest(uint256 _nftID) public payable onlyBuyer(_nftID) {
+        require(msg.value >= escrowAmount[_nftID]);
     }
 }
